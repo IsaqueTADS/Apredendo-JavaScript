96eaed3f78e11cba9128ac5433932de49d8045c9
IsaqueTADS <isaqueofc18@gmail.com>
2024-06-07 00:49:42 -0300

    .

---
+++ Java_Script/aula 11/ex008.js
@@ -0,0 +1,9 @@
+var vel = 250
+
+console.log(`A velocidade do seu carro é de ${vel}Km`)
+
+if (vel > 70 ){
+    console.log('Você ultrapassou o limite de velocidade \n Você SERÁ MULTADOO!!')
+    
+}
+//condição simples
---
+++ Java_Script/aula 11/ex009.js
@@ -0,0 +1,11 @@
+var numero = 15
+
+
+if (numero < 18){
+
+    console.log('Ele é menor de idade')
+
+}else {
+
+    console.log('Maior de idadae!')
+}
